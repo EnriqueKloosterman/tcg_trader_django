be2c1a89e5785448de2c13aeddc5d818003dfd70
EnriqueKloosterman <kloostermanen@gmail.com>
2024-10-15 18:47:37 -0300

    card detail page

--- card/views.py
+++ card/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Card, Card_Type, Faction
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def detail(request, pk):
+    card = get_object_or_404(Card, pk=pk)
+    related_items = Card.objects.filter(card_type=card.card_type, faction=card.faction, is_active=True).exclude(pk=pk)
+    
+    return render(request,'card/detail.html',{
+        'card': card,
+        'related_items': related_items
+    })
+
+
+@login_required
+def card_list(request):
+    cards = Card.objects.all()
+    return render(request, 'cards/card_list.html', {'cards': cards})
